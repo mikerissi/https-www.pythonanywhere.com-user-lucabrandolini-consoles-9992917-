57b3dbb5075082eb79ffbb89b1a4b9374e0d0028
lucabrandolini <rmike8328@gmail.com>
2018-08-28 18:00:49 +0000

    parlare con coloro che possono fare alcuna cosa

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,17 @@
 from flask import Flask, redirect, render_template, request, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="lucabrandolini",
+    password="Exeter22",
+    hostname="lucabrandolini.mysql.pythonanywhere-services.com",
+    databasename="lucabrandolini$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 comments = []
 
